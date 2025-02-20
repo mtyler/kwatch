2729f478258298a1baab46787f3ba489695b019e
mtyler <michael.tyler@gmail.com>
2025-02-20 14:12:39 -0500

    updates

--- watch-cluster.py
+++ watch-cluster.py
@@ -34,14 +34,23 @@ class Page:
     def refresh(self):
         self.display()
         cmd = rest()
-        if cmd == 'r':
+        if cmd == 'f':
             self.refresh()
-        elif cmd == 'x':
+        elif cmd == 'd':
             watch_nodes()
-        elif cmd == 'n':
+        elif cmd == 's':
             watch_ns()
-        elif cmd == 'c':
-            watch_checks()
+        elif cmd == 'a':
+            exec_checks()
+
+def rest():
+    cmd = ''
+    if do_ask:
+        cmd = input("\nCtrl+c exit | a=checks | s=ns | d=nodes | f=refresh:")
+    else:
+        time.sleep(interval)
+    return cmd
+
 
 def get_namespaces():
     result = subprocess.run(['kubectl', 'get', 'ns', '-o', 'jsonpath={.items[*].metadata.name}'], capture_output=True, text=True)
@@ -70,35 +79,100 @@ def watch(nodes):
 
 def watch_ns():
     while True:
-#        page = []
-#        page.append(("Rook Ceph Toolbox:", ['kubectl', 'logs', '-n', 'rook-ceph', 'jobs/rook-ceph-toolbox-job', 'script']))
-#        view_page('Storage Overview', page)
-        
+        # Get all namespaces and return details for each        
         namespaces = get_namespaces()
         for ns in namespaces:
             # Begin Page 1 for each namespace
             page = []
             page.append(("Pods:", ['kubectl', 'get', 'po', '-n', ns, '-o', 'wide']))
             page.append(("Services:", ['kubectl', 'get', 'svc', '-n', ns, '-o', 'wide']))
-#            page.append(("Pod Status:", ['kubectl', 'get', 'pods', '-o', 'custom-columns=POD:metadata.name,STATE:status.containerStatuses[*].state.waiting.reason', '-n', ns]))
-            
+            page.append(("Events:", ['kubectl', 'events', '-n', ns, '--types=Warning']))
+            ## Uncomment to display deployments and services
+            ##   page.append(("Deployments:", ['kubectl', 'get', 'deploy', '-n', ns, '-o', 'wide']))
+            ##   page.append(("Services:", ['kubectl', 'get', 'svc', '-n', ns, '-o', 'wide']))
             view_page(f"Namespace: {ns}", page)
-##            if ns in ['kube-public', 'kube-node-lease', 'default']:
-##                # Page 2&3 not needed in these namespaces for now
-##                continue
 
-####    Need a clean toggle for this page            
-####          # Begin Page 2 for each namespace
-##            if ns in ['kube-public', 'kube-node-lease', 'default']:
-##                # Page 2&3 not needed in these namespaces for now
-##                continue
-##            else:
-##                page = []
-##                page.append(("Deployments:", ['kubectl', 'get', 'deploy', '-n', ns, '-o', 'wide']))
-##                page.append(("Services:", ['kubectl', 'get', 'svc', '-n', ns, '-o', 'wide']))
-##                view_page(f'Namespace: {ns} 2', page)
+        # If auto scrolling is enabled, move to a watch nodes loop
+        if not do_ask:
+            watch_nodes()
 
-##### Helpful but not critical            
+def watch_nodes():
+    while True:
+        ## Show cluster overview
+        page = []
+        page.append(("Cluster Info:", ['kubectl', 'cluster-info']))
+        page.append(("Nodes:", ['kubectl', 'get', 'no', '-o', 'wide']))
+        page.append(("Conditions:", ['kubectl', 'get', 'no', '-o', 'jsonpath={range .items[*]}{.metadata.name}{": "}{range .status.conditions[*]}{.type}{"="}{.status}{" "}{end}{"\\n"}{end}']))
+        page.append(("Storage Classes:", ['kubectl', 'get', 'sc', '-o', 'wide']))
+        page.append(("Persistent Volumes:", ['kubectl', 'get', 'pv', '-o', 'wide']))
+        page.append(("Events:", ['kubectl', 'events', '--types=Warning', '-A']))
+            ## Uncomment to display allocated resources as displayed in the node status object
+        ## not very helpful...
+        #page.append(("Allocated Resources:", ['kubectl', 'get', 'no', '-o', 'jsonpath={range .items[*]}{.metadata.name}{": cpu "}{.status.allocatable.cpu}{" mem "}{.status.allocatable.memory}{" eph-storage "}{.status.allocatable.ephemeral-storage}{"\\n"}{end}']))
+        view_page('Cluster Overview', page)
+
+        ## Show allocated resources of all nodes in one page
+        page = []
+        nodes = get_nodes()
+        page.append(("Allocated:\tRequests\tLimits", ""))
+        for node in nodes:
+            page.append(("", f"echo {node} $(kubectl describe node {node} | grep 'Allocated' -A 8 | grep 'memory' | xargs)"))
+
+        for node in nodes:
+            page.append(("", f"echo {node} $(kubectl describe node {node} | grep 'Allocated' -A 8 | grep 'cpu' | xargs)"))
+        
+        view_page(f'Allocated Resources:', page)
+
+        ## Show node details
+        nodes = get_nodes()
+        for node in nodes:
+            page = []
+            page.append((f"Pods:", ['kubectl', 'get', 'po', f'--field-selector=spec.nodeName={node}', '-A', '-o', 'wide']))
+            #page.append(("Allocated Memory:\tRequests\tLimits", f"kubectl describe node {node} | grep 'Allocated' -A 8 | grep 'memory'"))
+            #page.append(("Allocated CPU:\t\tRequests\tLimits", f"kubectl describe node {node} | grep 'Allocated' -A 8 | grep 'cpu'"))
+            page.append(("Labels:", f"kubectl get node {node} -o jsonpath=\"{{.metadata.labels}}\""))
+            page.append(("Taints:", f"kubectl get node {node} -o jsonpath=\"{{range .spec.taints[*]}}{{.key}}={{.effect}} {{end}}\""))
+            view_page(f'Node: {node}', page)
+
+
+        # If auto scrolling is enabled, move to a watch namespaces loop
+        if not do_ask:
+            watch_ns()
+
+def exec_checks():
+    checks = [
+        # A list of "critical" services/resources to validate that cluster is operational
+        # Checks for kube-system components for errors
+        #('Control Plane System Time:', 'limactl shell cp1 timedatectl | grep time: | grep -E "Local|Universal"'),
+        ('Control Plane System Time:', 'echo "lima-cp1"; limactl shell cp1 timedatectl; echo "lima-n1"; limactl shell n1 timedatectl; echo "lima-n2"; limactl shell n2 timedatectl; echo "lima-n3"; limactl shell n3 timedatectl;'),
+        ('kube-system - kubeadm-config', 'kubectl get configmaps -n kube-system kubeadm-config -o yaml'),
+        ('kube-system - kube-apiserver - err logs:', 'kubectl logs -n kube-system -l component=kube-apiserver --tail=20 | grep err'),
+        ('kube-system - kube-controller-manager - err logs:', 'kubectl logs -n kube-system -l component=kube-controller-manager --tail=20 | grep err'),
+        ('kube-system - kube-scheduler - err logs:', 'kubectl logs -n kube-system -l component=kube-scheduler --tail=20 | grep err'),
+        ('kube-system - kube-proxy - err logs:', 'kubectl logs -n kube-system -l k8s-app=kube-proxy --tail=20 | grep err'),
+        # check bootstrap
+        ('cert-manager - Logs:', 'kubectl logs -n cert-manager -l app.kubernetes.io/instance=cert-manager --tail=20 | grep err'),
+        ('cert-manager - Cluster Issuer:', 'kubectl describe clusterissuers.cert-manager.io'),
+        ('cert-manager - Cluster CA:', 'kubectl describe certificaterequests.cert-manager.io -A'),
+        # check argocd
+        ('argocd - argocd-server - logs:', 'kubectl logs -n argocd -l app.kubernetes.io/name=argocd-server --tail=20'),
+        # check rook
+        ('rook-ceph - status:', ['kubectl', 'rook-ceph', 'ceph', 'status']),
+        # check monitoring
+        ('monitoring - prometheus-operator - all logs:', 'kubectl logs -n monitoring -l app.kubernetes.io/component=prometheus-operator --tail=20'),
+        ('monitoring - operator - init-config logs:', 'kubectl -n monitoring logs prometheus-prometheus-kube-prometheus-prometheus-0 init-config-reloader'),
+        ('monitoring - operator - config logs:', 'kubectl -n monitoring logs prometheus-prometheus-kube-prometheus-prometheus-0 config-reloader'),
+        ('monitoring - operator - prometheus logs:', 'kubectl -n monitoring logs prometheus-prometheus-kube-prometheus-prometheus-0 prometheus'),
+        ('monitoring - operator - kubectl exec -n monitoring -it prometheus-prometheus-kube-prometheus-prometheus-0 -c prometheus -- cat /etc/prometheus/config_out/prometheus.env.yaml:', 
+         'kubectl exec -n monitoring -it prometheus-prometheus-kube-prometheus-prometheus-0 -c prometheus -- cat /etc/prometheus/config_out/prometheus.env.yaml | grep "serviceMonitor"'),
+        # Checks for Network
+        ## check ingress
+        #('ingress-nginx - ingress-nginx-controller - logs:', 'kubectl logs -n ingress-nginx -l app.kubernetes.io/name=ingress-nginx --tail=20 | grep err'),
+        # check flannel
+        ('kube-flannel - kube-flannel - logs:', 'kubectl logs -n kube-flannel -l k8s-app=flannel -c kube-flannel --tail=20 | grep err'),
+        #('gateway:' , ['kubectl', 'logs', '-n', 'gateway', '-l', 'app.kubernetes.io/name=nginx-gateway-fabric', '-c', 'init' '--tail=20']),
+        #('metrics-server:', 'kubectl logs -n kube-system -l app.kubernetes.io/name=metrics-server --tail=20 | grep err'),
+        ##### Helpful but not critical            
 ##            # Begin Page 3 for each namespace
 ##            if ns == 'rook-ceph':
 ##                page = []
@@ -113,72 +187,19 @@ def watch_ns():
 ##                view_page(f'Namespace: {ns} 3', page)
             #elif ns == 'cert-manager':
             #    page.append(("Logs:", ['kubectl', 'logs', '-n', ns, '-l', "app.kubernetes.io/instance=cert-manager", '--tail=5']))
-        # If auto scrolling is enabled, move to a watch nodes loop
-        if not do_ask:
-            watch_nodes()
 
-def watch_nodes():
-    while True:
-        page = []
-        page.append(("Cluster Info:", ['kubectl', 'cluster-info']))
-        page.append(("Nodes:", ['kubectl', 'get', 'no', '-o', 'wide']))
-        page.append(("Conditions:", ['kubectl', 'get', 'no', '-o', 'jsonpath={range .items[*]}{.metadata.name}{": "}{range .status.conditions[*]}{.type}{"="}{.status}{" "}{end}{"\\n"}{end}']))
-        page.append(("Storage Classes:", ['kubectl', 'get', 'sc', '-o', 'wide']))
-        page.append(("Persistent Volumes:", ['kubectl', 'get', 'pv', '-o', 'wide']))
-        ## Uncomment to display allocated resources as displayed in the node status object
-        #page.append(("Allocated Resources:", ['kubectl', 'get', 'no', '-o', 'jsonpath={range .items[*]}{.metadata.name}{": cpu "}{.status.allocatable.cpu}{" mem "}{.status.allocatable.memory}{" eph-storage "}{.status.allocatable.ephemeral-storage}{"\\n"}{end}']))
-        view_page('Cluster Overview', page)
-
-        nodes = get_nodes()
-        for node in nodes:
-            page = []
-            page.append((f"Pods:", ['kubectl', 'get', 'po', f'--field-selector=spec.nodeName={node}', '-A', '-o', 'wide']))
-            page.append(("Allocated Memory:\tRequests\tLimits", f"kubectl describe node {node} | grep 'Allocated' -A 8 | grep 'memory'"))
-            page.append(("Allocated CPU:\t\tRequests\tLimits", f"kubectl describe node {node} | grep 'Allocated' -A 8 | grep 'cpu'"))
-            page.append(("Labels:", f"kubectl get node {node} -o jsonpath=\"{{.metadata.labels}}\""))
-            page.append(("Taints:", f"kubectl get node {node} -o jsonpath=\"{{range .spec.taints[*]}}{{.key}}={{.effect}} {{end}}\""))
-            view_page(f'Node: {node}', page)
-
-        # If auto scrolling is enabled, move to a watch namespaces loop
-        if not do_ask:
-            watch_ns()
-
-def watch_checks():
-    checks = [
-    ('gateway:' , ['kubectl', 'logs', '-n', 'gateway', '-l', 'app.kubernetes.io/name=nginx-gateway-fabric', '-c', 'init' '--tail=20']),
-    ('rook-ceph:', ['kubectl', 'logs', '-n', 'rook-ceph', '-l', 'app=rook-ceph-tools-operator-image']),
-    ('cert-manager:', 'kubectl logs -n cert-manager -l app.kubernetes.io/instance=cert-manager --tail=20 | grep err'),
-    ('kube-apiserver:', 'kubectl logs -n kube-system -l component=kube-apiserver --tail=20 | grep err'),
-    ('kube-controller-manager:', 'kubectl logs -n kube-system -l component=kube-controller-manager --tail=20 | grep err'),
-    ('kube-scheduler:', 'kubectl logs -n kube-system -l component=kube-scheduler --tail=20 | grep err'),
-    ('kube-proxy:', 'kubectl logs -n kube-system -l k8s-app=kube-proxy --tail=20 | grep err'),
-    ('metrics-server:', 'kubectl logs -n kube-system -l app.kubernetes.io/name=metrics-server --tail=20 | grep err'),
-    ('Cluster Issuer:', 'kubectl describe clusterissuers.cert-manager.io letsencrypt-staging-issuer')
     ]
     for check in checks:
         index = checks.index(check)
         sc_page = []
         sc_page.append(check)
-        view_page(f'Service Check {index + 1}/{len(checks)}:', sc_page)
+        view_page(f'System Check {index + 1}/{len(checks)}:', sc_page)
 
     # return to namespace watch loop
     watch_ns()
 
 
 
-##
-# TODO
-# Implement a method that shows deeper into cluster-wide resources
-# aka cluster roles, cluster role bindings, secrets, config maps, etc. 
-# def dig_deeper():
-#
-def rest():
-    cmd = ''
-    if do_ask:
-        cmd = input("\nCtrl+c exit | r=refresh | x=nodes | n=ns | c=checks | Press Enter to continue:")
-    else:
-        time.sleep(interval)
-    return cmd
     
 def signal_handler(sig, frame): 
     sys.exit(0)
